c9a2d3e8fe74486d1d882df248f522108c2fc09c
NIPE-SYSTEMS <dev.nipe.systems@gmail.com>
2018-01-17 14:25:12 +0100

    Add jupyter scripts

---
+++ examples/start-notebook.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+mkdir -p sharedfolder
+docker run -it --rm --name jupyter -p 8888:8888 -p 6006:6006 -v $(pwd):/root waleedka/modern-deep-learning jupyter notebook --ip=0.0.0.0 --allow-root
